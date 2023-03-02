eef9d4dfe634e2ebef27da564c294c58435f0a40
Divina-s <125366367+Divina-s@users.noreply.github.com>
2023-03-02 13:31:30 +0100

    MySite

--- mysite/mysite/MyApp/models.py
+++ mysite/mysite/MyApp/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-
+GENDER_CHOICES=(
+    ( "Male","Male"),
+    ("Female","Female"))
 class Intern(models.Model):
     First_name = models.CharField(max_length=15)
     Last_name = models.CharField(max_length=15)
     email =models.EmailField(max_length=20)
+    gender=models.IntegerField(choices=GENDER_CHOICES, default='1')
     dateofbirth =models.DateField()
 
         
